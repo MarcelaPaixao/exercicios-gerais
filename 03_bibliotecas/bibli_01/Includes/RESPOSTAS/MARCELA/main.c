54755e4ab996be854a9fcd7324ca5998c4ab5a02
MarcelaPaixao <142928839+MarcelaPaixao@users.noreply.github.com>
2023-09-05 10:55:26 -0300

    Delete 03_bibliotecas/bibli_01/Includes/

--- 03_bibliotecas/bibli_01/Includes/RESPOSTAS/MARCELA/main.c
+++
@@ -1,9 +0,0 @@
-#include "esfera_utils.h"
-#include <stdio.h>
-
-int main(){
-    float R=0;
-    scanf("%f", &R);
-    printf("Area: %.2f\nVolume: %.2f\n", calcula_area(R), calcula_volume(R));
-    return 0;
-}
