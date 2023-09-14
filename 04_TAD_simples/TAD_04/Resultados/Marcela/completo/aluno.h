3dfbe83cb3325566f1df60deafdfdcf746d56b8f
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2023-09-14 13:39:53 -0300

    TAD 04

--- 04_TAD_simples/TAD_04/Resultados/Marcela/completo/aluno.h
+++ 04_TAD_simples/TAD_04/Resultados/Marcela/completo/aluno.h
@@ -1,5 +1,11 @@
-#include "aluno.h"
-#include <stdio.h>
+#ifndef _ALUNO_H_
+#define _ALUNO_H_
+
+typedef struct Aluno {
+    char nome[50]; /**< Nome do aluno */
+    int matricula; /**< Matrícula do aluno */
+    int n1, n2, n3; /**< Notas das avaliações */
+} tAluno;
 
 /**
  * @brief Cria um novo aluno com os dados informados.
@@ -11,38 +17,14 @@
  * @param n3 Nota da terceira avaliação.
  * @return tAluno Retorna uma estrutura tAluno com os dados do aluno criado.
  */
-tAluno CriaAluno(char *nome, int matricula, int n1, int n2, int n3){
-    tAluno aluno;
-    int i = 0;
-    while(nome[i]){
-        aluno.nome[i] = nome[i];
-        i++;
-    }
-    aluno.nome[i] = '\0';
-    aluno.matricula = matricula;
-    aluno.n1 = n1;
-    aluno.n2 = n2;
-    aluno.n3 = n3;
-    return aluno;
-}
+tAluno CriaAluno(char *nome, int matricula, int n1, int n2, int n3); 
 
 /**
  * @brief Lê os dados de um aluno.
  * 
  * @return tAluno Retorna uma estrutura tAluno com os dados do aluno lido.
  */
-tAluno LeAluno(){
-    int matricula = 0, n1 = 0, n2 = 0, n3 = 0;
-    char nome[50];
-    tAluno aluno;
-
-    scanf("%s", nome);
-    scanf("%d", &matricula);
-    scanf("%d %d %d", &n1, &n2, &n3);
-
-    aluno = CriaAluno(nome, matricula, n1, n2, n3);
-    return aluno;
-}
+tAluno LeAluno();
 
 /**
  * @brief Compara as matrículas de dois alunos.
@@ -51,16 +33,7 @@ tAluno LeAluno(){
  * @param aluno2 Segundo aluno a ser comparado.
  * @return int Retorna 1 se a matrícula do aluno1 é maior que a do aluno2, -1 se a matrícula do aluno1 é menor que a do aluno2 e 0 se as matrículas são iguais.
  */
-int ComparaMatricula(tAluno aluno1, tAluno aluno2){
-    if(aluno1.matricula > aluno2.matricula){
-        return 1;
-    }
-    if(aluno1.matricula < aluno2.matricula){
-        return -1;
-    }
-    //Se chegou aqui, significa que as matriculas sao iguais
-    return 0;
-}
+int ComparaMatricula(tAluno aluno1, tAluno aluno2);
 
 /**
  * @brief Calcula a média das notas de um aluno.
@@ -68,9 +41,7 @@ int ComparaMatricula(tAluno aluno1, tAluno aluno2){
  * @param aluno Aluno a ter a média calculada.
  * @return int Retorna a média das notas do aluno.
  */
-int CalculaMediaAluno(tAluno aluno){
-    return (aluno.n1 + aluno.n2 + aluno.n3)/3;
-}
+int CalculaMediaAluno(tAluno aluno);
 
 /**
  * @brief Verifica se um aluno foi aprovado ou reprovado.
@@ -78,20 +49,13 @@ int CalculaMediaAluno(tAluno aluno){
  * @param aluno Aluno a ser verificado.
  * @return int Retorna 1 se o aluno foi aprovado e 0 se foi reprovado.
  */
-int VerificaAprovacao(tAluno aluno){
-    if(CalculaMediaAluno(aluno) >= 7){
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
+int VerificaAprovacao(tAluno aluno);
 
 /**
  * @brief Imprime os dados de um aluno.
  * 
  * @param aluno Aluno a ser impresso.
  */
-void ImprimeAluno(tAluno aluno){
-    printf("%s\n", aluno.nome);
-}
+void ImprimeAluno(tAluno aluno);
+
+#endif
