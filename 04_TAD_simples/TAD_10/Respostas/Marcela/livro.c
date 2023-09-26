fc8acf667fa90b30ec1713270d2869bc00cf6703
MarcelaPaixao <142928839+MarcelaPaixao@users.noreply.github.com>
2023-09-26 11:14:41 -0300

    Add files via upload

---
+++ 04_TAD_simples/TAD_10/Respostas/Marcela/livro.c
@@ -0,0 +1,27 @@
+#include "livro.h"
+#include <stdio.h>
+
+/**
+ * Lê os atributos de um livro a partir da entrada padrão (teclado).
+ *
+ * @return Um livro com os atributos lidos.
+ */
+tLivros lerLivro(){
+    tLivros livro;
+    scanf("%[^\n]%*c", livro.titulo);
+    scanf("%[^\n]%*c", livro.autor);
+    scanf("%d%*c", livro.anoPublicacao);
+    return livro;
+}
+
+/**
+ * Imprime os atributos da TAD tLivros (título, autor e ano de publicação).
+ * 
+ * @param livro O livro cujos atributos serão impressos
+*/
+void imprimeLivro(tLivros livro){
+    printf("Titulo: %s\n", livro.titulo);
+    printf("Autor: %s\n", livro.autor);
+    printf("Ano de Publicacao: %d\n", livro.anoPublicacao);
+}
+
