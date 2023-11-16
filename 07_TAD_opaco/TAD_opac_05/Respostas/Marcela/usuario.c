6301a1679b44fd0a82f5825010ef9071b3ddadd9
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2023-11-16 12:16:13 -0300

    opaco 5

---
+++ 07_TAD_opaco/TAD_opac_05/Respostas/Marcela/usuario.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "usuario.h"
+
+struct Usuario{
+    char *nome;
+    int cpf;
+};
+
+/**
+ * @brief Cria um novo usuário.
+ * Se não for possível alocar memória, o programa é encerrado.
+ * 
+ * @return Um ponteiro para o novo usuário criado.
+ */
+tUsuario *CriaUsuario(){
+    tUsuario *user = (tUsuario *) malloc (sizeof(tUsuario));
+    if(user == NULL){
+        printf("Falha na alocacao de memoria!\n");
+        exit(0);
+    }
+    user->nome = NULL;
+    user->cpf = 0;
+    return user;
+}
+
+/**
+ * @brief Desaloca a memória de um usuário.
+ * 
+ * @param user Ponteiro para o usuário a ser destruído.
+ */
+void DestroiUsuario(tUsuario *user){
+    if(user != NULL){
+        free(user->nome);
+        free(user);
+    }
+}
+
+/**
+ * @brief Lê os dados de um usuário da entrada padrão.
+ * 
+ * @param user Ponteiro para o usuário a ser lido.
+ */
+void LeUsuario(tUsuario *user){
+    char caractere = '\0';
+    int tam=0;
+    while(scanf("%c", &caractere) == 1 && caractere != '\n'){
+        tam++;
+        user->nome = realloc(user->nome, tam);
+        user->nome[tam - 1] = caractere;
+    }
+    user->nome = realloc(user->nome, tam + 1);
+    user->nome[tam] = '\0';
+
+    scanf("%d%*c", &user->cpf);
+}
+
+/**
+ * @brief Imprime os dados de um usuário.
+ * 
+ * @param user Ponteiro para o usuário a ser impresso.
+ */
+void ImprimeUsuario(tUsuario *user){
+    printf("Nome: %s\n", user->nome);
+    printf("CPF: %d\n", user->cpf);
+}
+
---
+++ 07_TAD_opaco/TAD_opac_05/Resultados/Marcela/completo/conta.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "usuario.h"
+#include "conta.h"
+
+struct Conta{
+    tUsuario *user;
+    int numConta;
+    float saldo;
+};
+
+/**
+ * @brief Cria uma nova conta.
+ * Se não for possível alocar memória, o programa é encerrado.
+ * 
+ * @return Um ponteiro para a nova conta criada.
+ */
+tConta *CriaConta(){
+    tConta *conta = (tConta *) malloc (sizeof(tConta));
+    if(conta == NULL){
+        printf("Falha na alocacao de memoria!\n");
+        exit(0);
+    }
+    conta->numConta = 0;
+    conta->saldo = 0;
+    conta->user = NULL;
+    return conta;
+}
+
+/**
+ * @brief Desaloca a memória de uma conta.
+ * 
+ * @param conta Ponteiro para a conta a ser destruída.
+ */
+void DestroiConta(tConta *conta){
+    if(conta != NULL){
+        DestroiUsuario(conta->user);
+        free(conta);
+    }
+}
+
+/**
+ * @brief Lê os dados de uma conta e do usuario atraves da entrada padrão.
+ * 
+ * @param conta Ponteiro para a conta a ser lida.
+ */
+void LeConta(tConta *conta){
+    conta->user = CriaUsuario();
+    LeUsuario(conta->user);
+    scanf("%d%*c", &conta->numConta);
+}
+
+/**
+ * @brief Imprime os dados de uma conta e do proprietário.
+ * 
+ * @param conta Ponteiro para a conta a ser impressa.
+ */
+void ImprimeConta(tConta *conta){
+    printf("Conta: %d\n", conta->numConta);
+    printf("Saldo: %.2f\n", conta->saldo);
+    ImprimeUsuario(conta->user);
+}
+
+/**
+ * @brief Verifica se uma conta possui o numero especificado.
+ * 
+ * @param conta Ponteiro para a conta a ser verificada.
+ * @param numero Número da conta a ser verificado.
+ * @return 1 se a conta possui o número especificado, 0 caso contrário.
+ */
+int VerificaConta(tConta *conta, int numero){
+    if(conta->numConta == numero){
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Realiza um saque em uma conta caso o saldo seja suficiente.
+ * 
+ * @param conta Ponteiro para a conta a ser sacada.
+ * @param valor Valor a ser sacado.
+ */
+void SaqueConta(tConta *conta, float valor){
+    if(conta->saldo >= valor){
+        conta->saldo -= valor;
+    }
+}
+
+/**
+ * @brief Realiza um depósito em uma conta.
+ * 
+ * @param conta Ponteiro para a conta a ser depositada.
+ * @param valor Valor a ser depositado.
+ */
+void DepositoConta(tConta *conta, float valor){
+    conta->saldo += valor;
+}
+
+/**
+ * @brief Realiza uma transferência entre duas contas, caso o saldo da conta de origem seja suficiente.
+ * 
+ * @param destino Ponteiro para a conta de destino.
+ * @param origem Ponteiro para a conta de origem.
+ * @param valor Valor a ser transferido.
+*/
+void TransferenciaConta(tConta *destino, tConta *origem, float valor){
+    if(origem->saldo >= valor){
+        SaqueConta(origem, valor);
+        DepositoConta(destino, valor);
+    }
+}
